09ebbba7e0fa94ac89a7f621fee1499aa99bcc07
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2023-05-04 17:53:16 +1200

    Add files via upload

---
+++ challenge11.js
@@ -0,0 +1,36 @@
+
+//   have to make integers return in a decending order 
+
+// //[1, 2, 3, 4, 5]
+// let n;
+
+// function descendingOrder(n){
+    
+// const digits = String(n)
+// .split('')
+// .map(Number);
+// return digits;
+// console.log(digits);
+
+
+
+function descendingOrder(n){
+    return parseInt(String(n).split('').sort().reverse().join(''))
+  }
+
+
+  }
+
+
+
+
+
+// let split 
+// var numArray = [140000];
+// numArray.sort(function(a, b) {
+//   return b - a;
+// });
+
+
+
+console.log(digits);
